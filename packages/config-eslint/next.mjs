a1204ff47bb78b5f8a8c4b84d28509f5762b6fd5
Luciana Kyoko <lukyoko@protonmail.com>
2026-07-28 10:21:03 -0300

    feat(web): integrate shared eslint configuration

--- packages/config-eslint/next.mjs
+++ packages/config-eslint/next.mjs
@@ -1,3 +1,6 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
 import base from "./base.mjs";
 
-export default [...base];
+export default [...base, ...nextVitals, ...nextTs];
