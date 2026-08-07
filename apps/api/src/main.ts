5494d0f64e20ba86c7556fd5a9de9779731d5382
Luciana Kyoko <lukyoko@protonmail.com>
2026-08-07 19:14:08 -0300

    chore(api): add application logging

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,12 +2,16 @@ import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger("Bootstrap");
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>("app.port");
-  await app.listen(port ?? 3001);
+  const port = configService.get<number>("app.port") ?? 3001;
+  await app.listen(port);
+
+  logger.log(`Application started on port ${port}`);
 }
 
 void bootstrap();
